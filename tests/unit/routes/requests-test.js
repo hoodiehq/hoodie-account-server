2b9c59d22bb4e5f5113b00be2a77bcca5b201e13
Garren Smith <garren.smith@gmail.com>
2015-10-29 15:57:14 +0200

    Password reset
    Create password reset is logic is basically complete
    Set password is a lot more rough

--- tests/unit/routes/requests-test.js
+++ tests/unit/routes/requests-test.js
@@ -2,7 +2,6 @@ var nock = require('nock')
 var test = require('tap').test
 
 var getServer = require('../utils/get-server')
-// var couchdbErrorTests = require('../utils/couchdb-error-tests')
 
 getServer(function (error, server) {
   if (error) {
@@ -13,6 +12,9 @@ getServer(function (error, server) {
     var postRequestRouteOptions = {
       method: 'POST',
       url: '/requests',
+      headers: {
+        authorization: 'Bearer 123'
+      },
       payload: {
         type: 'createresetpassword',
         username: 'john'
@@ -35,7 +37,7 @@ getServer(function (error, server) {
           uuids: ['9aef3ffae9597ce60dfec0fee200094c']
         })
         .put('/_users/org.couchdb.user%3Ajohn', function (body) {
-          return body.reset_token === '9aef3ffae9597ce60dfec0fee200094c'
+          return body.resetToken === '9aef3ffae9597ce60dfec0fee200094c'
         })
         .reply(200)
     }
@@ -49,11 +51,45 @@ getServer(function (error, server) {
       })
     })
 
+    function postMissingUserResponseMock () {
+      return nock('http://localhost:5984')
+        // mock valid response to sign in request
+        .get('/_users/org.couchdb.user%3Ajohn')
+        .reply(404)
+    }
+
+    group.test('sends error if user does not exist', function (t) {
+      t.plan(1)
+      postMissingUserResponseMock()
+      server.inject(postRequestRouteOptions, function (response) {
+        t.is(response.statusCode, 404)
+      })
+    })
+
+    function postMissingEmailUserResponseMock () {
+      return nock('http://localhost:5984')
+        // mock valid response to sign in request
+        .get('/_users/org.couchdb.user%3Ajohn')
+        .reply(200, {
+          _id: 'org.couchdb.user:john',
+          _rev: '1-abc',
+          name: 'john',
+          type: 'user',
+          roles: []
+        })
+    }
+
+    group.test('sends error if no email for user', function (t) {
+      t.plan(2)
+      postMissingEmailUserResponseMock()
+      server.inject(postRequestRouteOptions, function (response) {
+        t.is(response.statusCode, 404)
+        t.is(response.result.message, 'User does not have a stored email address.')
+      })
+    })
+
     /* missing tests:
-      * User doesn't exist
-      * User exists but no email
       * Send email notification with url
-      * errors on trying to create token
     */
     group.end()
   })
@@ -61,11 +97,12 @@ getServer(function (error, server) {
   test('POST /requests update password with token', function (group) {
     var postSetPasswordRequestRouteOptions = {
       method: 'POST',
-      url: '/requests',
+      url: '/passwordupdate',
+      headers: {
+        authorization: 'Token 9aef3ffae9597ce60dfec0fee200094c'
+      },
       payload: {
-        type: 'setpassword',
         username: 'john',
-        token: '9aef3ffae9597ce60dfec0fee200094c',
         password: 'this-is-my-password'
       }
     }
@@ -77,26 +114,57 @@ getServer(function (error, server) {
           _id: 'org.couchdb.user:john',
           _rev: '1-abc',
           name: 'john',
+          resetToken: '9aef3ffae9597ce60dfec0fee200094c',
+          resetTimeStamp: new Date(),
           type: 'user',
           email: 'john@email.com',
           roles: []
         })
-        .get('/uuids')
-        .reply(200, {
-          uuids: ['9aef3ffae9597ce60dfec0fee200094c']
-        })
         .put('/_users/org.couchdb.user%3Ajohn', function (body) {
-          return body.reset_token === '9aef3ffae9597ce60dfec0fee200094c'
+          return body.password === 'this-is-my-password'
         })
         .reply(200)
     }
+
     group.test('Sets password for user with correct token', function (t) {
+      t.plan(1)
+      postSetPasswordRequestResponseMock()
+
       server.inject(postSetPasswordRequestRouteOptions, function (response) {
-        t.is(response.result.msg, 'password updated')
+        t.is(response.result.msg, 'Password has been updated.')
       })
+    })
 
-    });
+    function postSetPasswordBadTokenRequestResponseMock () {
+      return nock('http://localhost:5984')
+        // mock valid response to sign in request
+        .get('/_users/org.couchdb.user%3Ajohn')
+        .reply(200, {
+          _id: 'org.couchdb.user:john',
+          _rev: '1-abc',
+          name: 'john',
+          resetToken: '123123',
+          resetTimeStamp: new Date(),
+          type: 'user',
+          email: 'john@email.com',
+          roles: []
+        })
+        .put('/_users/org.couchdb.user%3Ajohn', function (body) {
+          return body.password === 'this-is-my-password'
+        })
+        .reply(200)
+    }
 
-    group.end();
-  });
+    group.test('sends error if token does not match', function (t) {
+      t.plan(2)
+      postSetPasswordBadTokenRequestResponseMock()
+
+      server.inject(postSetPasswordRequestRouteOptions, function (response) {
+        t.is(response.result.message, 'Incorrect password token supplied.')
+        t.is(response.statusCode, 400)
+      })
+    })
+
+    group.end()
+  })
 })
