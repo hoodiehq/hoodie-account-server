d7b11091b1d8f422588d837a6be555b9cc209f63
Gregor Martynus <gregor@martynus.net>
2016-02-29 23:51:10 -0500

    fix(routes): PATCH /session/account/profile?include=foobar
    * * *
    
    This commit was sponsored by Neighbourhoodie
    
    You can hire Neighbourhoodie for all your
    Hoodie / CouchDB / Offline First needs
    http://go.hood.ie/thehoodiefirm

--- routes/profile.js
+++ routes/profile.js
@@ -78,6 +78,7 @@ function profileRoutes (server, options, next) {
       validate: {
         headers: validations.bearerTokenHeader,
         payload: validations.profilePayload,
+        query: validations.profileQuery,
         failAction: joiFailAction
       }
     },
