23acafa9c10e0e6725e58250e5550d4f60cd35c3
sebbel <sebastian@pancia.de>
2016-04-12 19:19:17 +0200

    test: remove cookie from requests

---
+++ tests/integration/routes/no-cookie-header-test.js
@@ -0,0 +1,59 @@
+var test = require('tap').test
+
+var getServer = require('../utils/get-server')
+
+var routeOptions = {
+  method: 'GET',
+  url: '/',
+  headers: {
+    cookie: 'testCookie'
+  }
+}
+
+var routePOSTOptions = {
+  method: 'POST',
+  url: '/',
+  headers: {
+    cookie: 'testCookie'
+  }
+}
+
+test('Recieve no cookie on GET Request', function (group) {
+  getServer({
+  }, function (error, server) {
+    if (error) {
+      return test('test setup', function (t) {
+        t.error(error)
+        t.end()
+      })
+    }
+
+    group.test('GET no cookie', function (t) {
+      server.inject(routeOptions, function (response) {
+        t.is(response.raw.req.headers.cookie, undefined, 'Header Cookie on GET Request should be null')
+        t.end()
+      })
+      group.end()
+    })
+  })
+})
+
+test('Recieve no cookie on POST Request', function (group) {
+  getServer({
+  }, function (error, server) {
+    if (error) {
+      return test('test setup', function (t) {
+        t.error(error)
+        t.end()
+      })
+    }
+
+    group.test('POST no cookie', function (t) {
+      server.inject(routePOSTOptions, function (response) {
+        t.is(response.raw.req.headers.cookie, undefined, 'Header Cookie on GET Request should be null')
+        t.end()
+      })
+      group.end()
+    })
+  })
+})
