1a7a9b608bae507dbb5303ccb4fb979977526219
James West <james@jameswest.co.nz>
2016-06-02 15:34:04 +1200

    Proof of concept for account profiles.

--- api/utils/doc-to-account.js
+++ api/utils/doc-to-account.js
@@ -16,11 +16,8 @@ function toAccount (doc, options) {
   var account = {
     id: accountId,
     username: username,
+    profile: doc.profile || {},
     roles: findCustomRoles(roles)
-  }
-
-  if (options.includeProfile) {
-    account.profile = doc.profile
   }
 
   return account
--- routes/account.js
+++ routes/account.js
@@ -34,11 +34,13 @@ function accountRoutes (server, options, next) {
     handler: function (request, reply) {
       var username = request.payload.data.attributes.username
       var password = request.payload.data.attributes.password
+      var profile = request.payload.data.attributes.profile || {}
       var id = request.payload.data.id
       var query = request.query
       accounts.add({
         username: username,
         password: password,
+        profile: profile,
         include: query.include,
         id: id
       })
