c111f172f5a7e2e3f22e29ecb89d307ecc9a898f
Gregor <gregor@martynus.net>
2016-10-14 23:21:05 -0700

    test: options.PouchDB

--- tests/integration/utils/get-server.js
+++ tests/integration/utils/get-server.js
@@ -3,7 +3,9 @@ module.exports = getServer
 var defaults = require('lodash/defaultsDeep')
 var Hapi = require('hapi')
 var nock = require('nock')
-var PouchDB = require('pouchdb')
+var PouchDB = require('pouchdb').defaults({
+  prefix: 'http://localhost:5984/'
+})
 
 var hapiAccount = require('../../../plugin')
 
@@ -31,26 +33,19 @@ function getServer (options, callback) {
     .put('/_users/_design/byId')
     .reply(201)
 
-  PouchDB.plugin(require('pouchdb-users'))
-
-  var usersDb = new PouchDB('http://localhost:5984/_users')
-  usersDb.installUsersBehavior()
-  .then(function () {
-    server.register({
-      register: hapiAccount,
-      options: defaults({
-        usersDb: usersDb,
-        secret: 'secret',
-        admins: {
-          // -<password scheme>-<derived key>,<salt>,<iterations>
-          // password is "secret"
-          admin: '-pbkdf2-a2ca9d3ee921c26d2e9d61e03a0801b11b8725c6,1081b31861bd1e91611341da16c11c16a12c13718d1f712e,10'
-        },
-        notifications: {}
-      }, options)
-    }, function (error) {
-      callback(error, server)
-    })
+  server.register({
+    register: hapiAccount,
+    options: defaults({
+      PouchDB: PouchDB,
+      secret: 'secret',
+      admins: {
+        // -<password scheme>-<derived key>,<salt>,<iterations>
+        // password is "secret"
+        admin: '-pbkdf2-a2ca9d3ee921c26d2e9d61e03a0801b11b8725c6,1081b31861bd1e91611341da16c11c16a12c13718d1f712e,10'
+      },
+      notifications: {}
+    }, options)
+  }, function (error) {
+    callback(error, server)
   })
-  .catch(callback)
 }
